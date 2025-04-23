b01a6cdf42e6c998b82f6958803fc50bdb284eec
KAChamika <tharuprocessing.chamika@gmail.com>
2025-04-23 08:18:19 +0530

    Adding theme

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import { ConfigProvider } from 'antd';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
-import { blue } from './lib/utils/colors';
-import { primaryFont } from './lib/utils/typography';
+import { theme } from './lib/utils/theme';
+import { AuthenticationProvider } from './providers/AuthenticationProvider';
 
 import './styles/globals.css';
 
@@ -11,24 +11,9 @@ const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
 root.render(
-    <ConfigProvider theme={{
-        token: {
-            colorPrimary: blue[300],
-            fontFamily: primaryFont,
-        },
-        components: {
-            Typography: {
-                titleMarginTop: 0,
-                titleMarginBottom: 0,
-            },
-            Input: {
-                paddingBlock: 10,
-                paddingInline: 16,
-                hoverBorderColor: blue[200],
-                activeBorderColor: blue[200]
-            }
-        }
-    }}>
-        <App />
-    </ConfigProvider>
+    <AuthenticationProvider>
+        <ConfigProvider theme={theme}>
+            <App />
+        </ConfigProvider>
+    </AuthenticationProvider>
 );
---
+++ src/lib/utils/theme.ts
@@ -0,0 +1,21 @@
+import { blue } from './colors';
+import { primaryFont } from './typography';
+
+export const theme = {
+    token: {
+        colorPrimary: blue[300],
+        fontFamily: primaryFont,
+    },
+    components: {
+        Typography: {
+            titleMarginTop: 0,
+            titleMarginBottom: 0,
+        },
+        Input: {
+            paddingBlock: 10,
+            paddingInline: 16,
+            hoverBorderColor: blue[200],
+            activeBorderColor: blue[200]
+        }
+    }
+}
